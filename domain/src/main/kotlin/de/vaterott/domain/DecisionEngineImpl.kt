5c6739885ea3bc4a60d9ca6c47cfbefc35d1dcdf
Marcel <m_v@gmx.de>
2025-02-25 00:01:51 +0100

    feat(Kotlin Rules Engine Lite): Instances instead of List for kotlin

--- domain/src/main/kotlin/de/vaterott/domain/DecisionEngineImpl.kt
+++ domain/src/main/kotlin/de/vaterott/domain/DecisionEngineImpl.kt
@@ -6,12 +6,13 @@ import de.vaterott.api.korulite.CountryDecisionRuleProvider
 import de.vaterott.api.domain.Decision
 import de.vaterott.api.domain.Person
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.inject.Instance
 import jakarta.inject.Inject
 
 // Todo Until now, the DecisionEngineImpl only deals with CountryDecisionRuleProviders.. should be concretized
 @ApplicationScoped
 class DecisionEngineImpl @Inject constructor(
-    private val ruleProviders: List<CountryDecisionRuleProvider>
+    private val ruleProviders: Instance<CountryDecisionRuleProvider>
 ) {
     fun evaluate(decision: Decision, person: Person): Boolean {
         val provider = ruleProviders.firstOrNull { it.supports(person.country) }
