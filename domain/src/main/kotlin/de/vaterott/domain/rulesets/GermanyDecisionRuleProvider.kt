e731653b9035811256361e1a1a4259ce2aa3d1bb
Marcel Vaterott <m.vaterott@prosoz.de>
2025-02-25 08:22:18 +0100

    refactor(DecisionRuleProvider): Get rid off the country boundaries

--- domain/src/main/kotlin/de/vaterott/domain/rulesets/GermanyDecisionRuleProvider.kt
+++ domain/src/main/kotlin/de/vaterott/domain/rulesets/GermanyDecisionRuleProvider.kt
@@ -1,8 +1,8 @@
 package de.vaterott.domain.rulesets
 
 import de.vaterott.api.domain.Country
-import de.vaterott.api.korulite.CountryDecisionRuleProvider
 import de.vaterott.api.domain.Decision
+import de.vaterott.api.korulite.DecisionRuleProvider
 import de.vaterott.api.korulite.RuleSet
 import de.vaterott.api.korulite.ruleSet
 import de.vaterott.domain.rules.LastNamePrefixRule
@@ -10,22 +10,22 @@ import de.vaterott.domain.rules.MinAgeRule
 import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class GermanyDecisionRuleProvider : CountryDecisionRuleProvider {
+class GermanyDecisionRuleProvider: DecisionRuleProvider<Country> {
 
-    override fun supports(country: Country): Boolean = country == Country.DE
+    override fun supports(enum: Country): Boolean = enum == Country.DE
 
     // Todo Refactor rules to be more readable
-    private val decisionRules: Map<Decision, RuleSet> = mapOf(
-        Decision.CAN_DRINK_BEER to ruleSet {
+    private val decisionRules: Map<String, RuleSet> = mapOf(
+        Decision.CAN_DRINK_BEER.name to ruleSet {
             rule(MinAgeRule(16))
             rule(LastNamePrefixRule("S"))
         },
-        Decision.CAN_DRIVE_RIGHT to ruleSet {
+        Decision.CAN_DRIVE_RIGHT.name to ruleSet {
             rule(MinAgeRule(18))
         }
     )
 
-    override fun getRuleSet(decision: Decision): RuleSet =
+    override fun getRuleSet(decision: String): RuleSet =
         decisionRules[decision]
             ?: throw IllegalArgumentException("Decision $decision not supported for Germany.")
 }
--- domain/src/main/kotlin/de/vaterott/domain/rulesets/USADecisionRuleProvider.kt
+++ domain/src/main/kotlin/de/vaterott/domain/rulesets/USADecisionRuleProvider.kt
@@ -1,29 +1,29 @@
 package de.vaterott.domain.rulesets
 
 import de.vaterott.api.domain.Country
-import de.vaterott.api.korulite.CountryDecisionRuleProvider
 import de.vaterott.api.domain.Decision
+import de.vaterott.api.korulite.DecisionRuleProvider
 import de.vaterott.api.korulite.RuleSet
 import de.vaterott.api.korulite.ruleSet
 import de.vaterott.domain.rules.MinAgeRule
 import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class USADecisionRuleProvider : CountryDecisionRuleProvider {
+class USADecisionRuleProvider : DecisionRuleProvider<Country> {
 
-    override fun supports(country: Country): Boolean = country == Country.USA
+    override fun supports(enum: Country): Boolean = enum == Country.USA
 
     // Todo Refactor rules to be more readable
-    private val decisionRules: Map<Decision, RuleSet> = mapOf(
-        Decision.CAN_DRINK_BEER to ruleSet {
+    private val decisionRules: Map<String, RuleSet> = mapOf(
+        Decision.CAN_DRINK_BEER.name to ruleSet {
             rule(MinAgeRule(18))
         },
-        Decision.CAN_DRIVE_RIGHT to ruleSet {
+        Decision.CAN_DRIVE_RIGHT.name to ruleSet {
             rule(MinAgeRule(16))
         }
     )
 
-    override fun getRuleSet(decision: Decision): RuleSet =
+    override fun getRuleSet(decision: String): RuleSet =
         decisionRules[decision]
             ?: throw IllegalArgumentException("Decision $decision not supported for USA.")
 }
